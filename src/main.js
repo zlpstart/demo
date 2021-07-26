4bc8c6571c3251cf5cf36691f1ada9813b7925c0
e <844023644@qq.com>
2021-07-26 21:07:54 +0800

    分支练习

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+
+console.log(process.env.VUE_APP_BASE_URL)
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
---
+++ src/views/Home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="block">
+      <span class="demonstration">默认</span>
+      <el-date-picker
+        v-model="value1"
+        type="daterange"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        value-format="yyyy/MM/dd"
+      >
+      </el-date-picker>
+    </div>
+    <p>{{ value1 }}</p>
+    <h1>{{baseUrl}}</h1>
+    <h1>{{upload}}</h1>
+    <p>我是Master</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value1: "",
+      baseUrl:process.env.VUE_APP_BASE_URL,
+      upload:process.env.VUE_APP_UPLOAD,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
